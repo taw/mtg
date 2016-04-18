e131336a0499478efa1ab1691a8f9746a3e3b4c5
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2016-04-18 12:02:48 +0100

    mtgtop8 test

--- spec/url2cod_spec.rb
+++ spec/url2cod_spec.rb
@@ -6,24 +6,34 @@ require "pry"
 
 describe "url2cod" do
   let(:binary) { Pathname(__dir__)+"../bin/url2cod" }
+  let(:run) do
+    Dir.chtmpdir do |tmp_dir|
+      system "ruby", "-r#{__dir__}/mock_network", binary.to_s, url
+      system "diff -r #{tmp_dir} #{__dir__}/url2cod/#{reference_dir}/"
+    end
+  end
 
   describe "www.wizards.com" do
     let(:url) { "http://magic.wizards.com/en/articles/archive/top-decks/evolving-mana-bases-2016-04-15" }
+    let(:reference_dir) { "wizards_com" }
     it do
-      Dir.chtmpdir do |tmp_dir|
-        system "ruby", "-r#{__dir__}/mock_network", binary.to_s, url
-        system "diff -r #{tmp_dir} #{__dir__}/url2cod/wizards_com"
-      end
+      run
     end
   end
 
   describe "SCG" do
     let(:url) { "http://sales.starcitygames.com/deckdatabase/displaydeck.php?DeckID=101059" }
+    let(:reference_dir) { "scg" }
     it do
-      Dir.chtmpdir do |tmp_dir|
-        system "ruby", "-r#{__dir__}/mock_network", binary.to_s, url
-        system "diff -r #{tmp_dir} #{__dir__}/url2cod/scg"
-      end
+      run
+    end
+  end
+
+  describe "mtgtop8" do
+    let(:url) { "http://mtgtop8.com/event?e=11914&f=MO" }
+    let(:reference_dir) { "mtgtop8" }
+    it do
+      run
     end
   end
 end
