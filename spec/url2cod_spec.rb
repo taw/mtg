bc942a72b969bb327266955f8ea361bca1bee89a
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-08-29 15:38:31 +0100

    Oops, integration tests were not actually running

--- spec/url2cod_spec.rb
+++ spec/url2cod_spec.rb
@@ -2,8 +2,8 @@ describe "url2cod" do
   let(:binary) { Pathname(__dir__)+"../bin/url2cod" }
   let(:run) do
     Dir.chtmpdir do |tmp_dir|
-      system "ruby", "-r#{__dir__}/mock_network", binary.to_s, url
-      system "diff -r #{tmp_dir} #{__dir__}/url2cod/#{reference_dir}/"
+      system "ruby", "-r#{__dir__}/mock_network", binary.to_s, url or raise "Script failed"
+      system "diff -r #{tmp_dir} #{__dir__}/url2cod/#{reference_dir}/" or raise "Diff failed"
     end
   end
 
